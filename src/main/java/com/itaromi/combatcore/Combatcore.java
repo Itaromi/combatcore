5a895f6255f8869806805a73365a3dce25f33811
Youn Sylvestre <sylvestre.youn@gmail.com>
2025-01-07 23:40:28 +0100

    feat: add sout + author

--- src/main/java/com/itaromi/combatcore/Combatcore.java
+++ src/main/java/com/itaromi/combatcore/Combatcore.java
@@ -7,11 +7,13 @@ public final class Combatcore extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Combatcore: Plugin enabled");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Combatcore: Plugin disabled");
     }
 }
